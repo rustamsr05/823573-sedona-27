e24c932dcb4a17547b77f3928dce399852e3682d
rustamsr05 <rustamsr05@gmail.com>
2020-03-18 13:39:27 +0600

    Задание 8

--- js/form.js
+++ js/form.js
@@ -2,7 +2,14 @@ var link = document.querySelector(".button-search");
 
 var popup = document.querySelector(".hotel-searching-form");
 
+popup.classList.add("form-remove");
+
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.add("form-remove");
+  if(popup.classList.contains("form-remove")) {
+    popup.classList.remove("form-remove");
+  } else {
+      popup.classList.add("form-remove");
+    }
+
 });
